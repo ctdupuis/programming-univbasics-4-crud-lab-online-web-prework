982359a9fb5520c71a1626bb682dbbd2154f82aa
Cody Dupuis <ctdupuis7@gmail.com>
2019-08-21 00:15:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,8 @@ array = ["elm0","elm1","elm2","elm3"]
 end
 
 def add_element_to_end_of_array(array, element)
-  array = ["arrays!"]
-  array.push("array?")
+  array = []
+  array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
