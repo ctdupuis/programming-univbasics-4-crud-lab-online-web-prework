364a4fcd003934c072ae03732194d9af077b8406
Cody Dupuis <ctdupuis7@gmail.com>
2019-08-21 00:19:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,7 @@ end
 
 def remove_element_from_end_of_array(array)
   array = ["arrays!","rm_arrays"]
-  rm_array = array.pop
+  rm_arrays = array.pop
 end
 
 def remove_element_from_start_of_array(array)
